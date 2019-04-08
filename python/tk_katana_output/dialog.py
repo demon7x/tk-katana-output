0d25f2cc8cfc0b2d986c9d1e1d2bcc6e6ce82d0e
demon7x <demon7x@westworld.co.kr>
2019-04-08 17:34:53 +0900

    only local test

---
+++ python/tk_katana_output/dialog.py
@@ -0,0 +1,112 @@
+# Copyright (c) 2013 Shotgun Software Inc.
+# 
+# CONFIDENTIAL AND PROPRIETARY
+# 
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your 
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+
+import subprocess
+
+import sgtk
+
+
+from sgtk.platform.qt import QtCore, QtGui
+from .ui.dialog import Ui_Dialog
+from sgtk import TankError
+
+
+
+from Katana import NodegraphAPI
+from Katana import Utils
+from Katana import FarmAPI
+from Katana import KatanaFile
+
+
+
+def show_dialog(app_instance):
+    """
+    Shows the main dialog window.
+    """
+    # in order to handle UIs seamlessly, each toolkit engine has methods for launching
+    # different types of windows. By using these methods, your windows will be correctly
+    # decorated and handled in a consistent fashion by the system. 
+    
+    # we pass the dialog class to this method and leave the actual construction
+    # to be carried out by toolkit.
+    app_instance.engine.show_dialog("Output", app_instance, AppDialog)
+    
+
+
+class AppDialog(QtGui.QWidget):
+    """
+    Main application dialog window
+    """
+    
+    def __init__(self):
+        """
+        Constructor
+        """
+        QtGui.QWidget.__init__(self)
+        
+        self.ui = Ui_Dialog() 
+        self.ui.setupUi(self)
+        
+        self._app = sgtk.platform.current_bundle()
+
+        
+        self._set_frame()
+        self._set_select_node()
+        
+        #callback
+        Utils.EventModule.RegisterEventHandler(self._set_select_node, 'node_setSelected')
+
+        #self.ui.tail_lineedit.textChanged.connect(self._edit_tail)
+        self.ui.output_btn.clicked.connect(self._render)
+    
+    
+
+    def _set_frame(self):
+        start_time = NodegraphAPI.NodegraphGlobals.GetWorkingInTime()
+        end_time = NodegraphAPI.NodegraphGlobals.GetWorkingOutTime()
+
+        self.ui.start_frame.setText(str(start_time))
+        self.ui.end_frame.setText(str(end_time))
+        
+
+    def _set_select_node(self,eventType=None,eventID=None,node=None):
+        
+        if not node:
+            node =  NodegraphAPI.GetAllSelectedNodes()[0]
+        self.ui.sel_node.setText(node.getName())
+        return 
+
+    def _render(self):
+        import os
+        
+        file_name = FarmAPI.GetKatanaFileName()
+        
+        #tmp_file = os.path.join("/tmp",file_name.split("/")[-1])
+        #KatanaFile.Save(tmp_file)
+
+
+        command = ['mate-terminal','rez-env','katana-3.1.2','renderman-22','usd','--','katana']
+        
+        command.append("--batch")
+        command.append("--katana-file=%s"%file_name)
+        command.append("--render-node=%s"%self.ui.sel_node.text())
+        command.append(str("--t=%s-%s"%(self.ui.start_frame.text(),self.ui.end_frame.text())))
+
+        subprocess.Popen(command)
+        self.close()
+        return
+
+
+    
+    def closeEvent(self,event):
+        
+        Utils.EventModule.UnregisterEventHandler(self._set_select_node, 'node_setSelected')
+    
---
+++ python/tk_katana_output/ui/dialog.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'dialog.ui'
+#
+#      by: pyside-uic 0.2.15 running on PySide 1.2.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from tank.platform.qt import QtCore, QtGui
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(515, 156)
+        self.verticalLayout = QtGui.QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout_3 = QtGui.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.label_6 = QtGui.QLabel(Dialog)
+        self.label_6.setObjectName("label_6")
+        self.horizontalLayout_3.addWidget(self.label_6)
+        self.sel_node = QtGui.QLineEdit(Dialog)
+        self.sel_node.setObjectName("sel_node")
+        self.horizontalLayout_3.addWidget(self.sel_node)
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
+        self.groupBox_3 = QtGui.QGroupBox(Dialog)
+        self.groupBox_3.setObjectName("groupBox_3")
+        self.horizontalLayout = QtGui.QHBoxLayout(self.groupBox_3)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout_8 = QtGui.QVBoxLayout()
+        self.verticalLayout_8.setObjectName("verticalLayout_8")
+        self.label_5 = QtGui.QLabel(self.groupBox_3)
+        self.label_5.setObjectName("label_5")
+        self.verticalLayout_8.addWidget(self.label_5)
+        self.start_frame = QtGui.QLineEdit(self.groupBox_3)
+        self.start_frame.setText("")
+        self.start_frame.setObjectName("start_frame")
+        self.verticalLayout_8.addWidget(self.start_frame)
+        self.horizontalLayout.addLayout(self.verticalLayout_8)
+        self.verticalLayout_9 = QtGui.QVBoxLayout()
+        self.verticalLayout_9.setObjectName("verticalLayout_9")
+        self.label_7 = QtGui.QLabel(self.groupBox_3)
+        self.label_7.setObjectName("label_7")
+        self.verticalLayout_9.addWidget(self.label_7)
+        self.end_frame = QtGui.QLineEdit(self.groupBox_3)
+        self.end_frame.setObjectName("end_frame")
+        self.verticalLayout_9.addWidget(self.end_frame)
+        self.horizontalLayout.addLayout(self.verticalLayout_9)
+        self.verticalLayout.addWidget(self.groupBox_3)
+        self.output_btn = QtGui.QPushButton(Dialog)
+        self.output_btn.setObjectName("output_btn")
+        self.verticalLayout.addWidget(self.output_btn)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "The Current Sgtk Environment", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_6.setText(QtGui.QApplication.translate("Dialog", "Select Node", None, QtGui.QApplication.UnicodeUTF8))
+        self.groupBox_3.setTitle(QtGui.QApplication.translate("Dialog", "Frame Range", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_5.setText(QtGui.QApplication.translate("Dialog", "Start", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_7.setText(QtGui.QApplication.translate("Dialog", "End", None, QtGui.QApplication.UnicodeUTF8))
+        self.output_btn.setText(QtGui.QApplication.translate("Dialog", "Output", None, QtGui.QApplication.UnicodeUTF8))
+
+from . import resources_rc
